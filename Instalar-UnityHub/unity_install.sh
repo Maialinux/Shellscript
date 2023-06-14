36b78f1ceacf6d784c685d33682fbc7ab287b763
Maialinux <133462458+Maialinux@users.noreply.github.com>
2023-06-14 11:54:37 -0300

    Update unity_install.sh

--- Instalar-UnityHub/unity_install.sh
+++ Instalar-UnityHub/unity_install.sh
@@ -3,4 +3,4 @@
 wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list
 apt update -y
-#apt install unityhub
+apt install unityhub
